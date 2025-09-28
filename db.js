85284a78bb0a26655ecfc794ccc159e6090aa0d7
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-09-28 14:25:13 +0000

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,16 @@
-import mongoose from 'mongoose';
-import config from './config.js';
+import mongoose from "mongoose";
 
-mongoose.connect(config.MONGO_URI, {
+await mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.log("❌ MongoDB error", err));
-
-const userSchema = new mongoose.Schema({
-  telegram_id: Number,
-  username: String,
-  premium: { type: Boolean, default: false },
-  panels_used: { type: Number, default: 0 },
-  panels_limit: { type: Number, default: 5 },
-  expire_at: Date,
+  useUnifiedTopology: true,
 });
 
-export const Users = mongoose.model('Users', userSchema);
+const userSchema = new mongoose.Schema({
+  telegram_id: String,
+  premium: { type: Boolean, default: false },
+  expire_at: Date, // date d'expiration du statut premium
+  panels_used: { type: Number, default: 0 },
+  panels_limit: { type: Number, default: 5 }
+});
+
+export const Users = mongoose.model("Users", userSchema);
