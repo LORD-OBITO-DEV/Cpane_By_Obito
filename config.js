8b6350830f74a27f2907a1426ff37164e1d4eee1
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-09-28 16:16:21 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,40 +1,39 @@
+import 'dotenv/config';
+
 export default {
-  BOT_TOKEN: "8133998553:AAGJGie0NxO5Aie_R7cs_cz4UHSFgtkN_Pw",
-  ADMIN_ID: 6587556123,
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  ADMIN_ID: Number(process.env.ADMIN_ID),
+  USE_WHATSAPP: process.env.USE_WHATSAPP === 'true',
+  ADMIN_NUMBER: process.env.ADMIN_NUMBER,
 
-USE_WHATSAPP: true,
-    CONTACT_ADMIN: {
-        telegram: "https://t.me/LORD_OBITO_DEV",
-        whatsapp: "+2256587556123"
-    },
+  PTERO_URL: process.env.PTERO_URL,
+  PTERO_API_KEY: process.env.PTERO_API_KEY,
+  NEST_ID: Number(process.env.NEST_ID),
+  EGG_ID: Number(process.env.EGG_ID),
+  PANEL_DEFAULT_RAM: Number(process.env.PANEL_DEFAULT_RAM),
+  PANEL_DEFAULT_CPU: Number(process.env.PANEL_DEFAULT_CPU),
+  PANEL_DEFAULT_DISK: Number(process.env.PANEL_DEFAULT_DISK),
 
-    NEWSLETTER: [
-        "120363401981326696@newsletter",
-        "120363419984097704@newsletter"
-    ],
+  ADSTERA_LINK: process.env.ADSTERA_LINK,
+  MENU_IMG: process.env.MENU_IMG,
 
-ADMIN_NUMBER: "2250712668494", // Numéro WhatsApp de l'admin
-  
-  PTERO_URL: "https://panel.tondomaine.com",
-  PTERO_API_KEY: "ptla_P5ajM5606J9cvfqbueud8582blHWqkDevte2NwRNLOq",
-  NEST_ID: 1,
-  EGG_ID: 5,
-  PANEL_DEFAULT_RAM: 0,
-  PANEL_DEFAULT_CPU: 0,
-  PANEL_DEFAULT_DISK: 0,
+  CONTACT_ADMIN: {
+    telegram: process.env.CONTACT_ADMIN_TELEGRAM,
+    whatsapp: process.env.CONTACT_ADMIN_WHATSAPP,
+    phone: process.env.CONTACT_ADMIN_PHONE
+  },
 
-  ADSTERA_LINK: "https://tonlien.adstera.com",
+  MONGO_URI: process.env.MONGO_URI,
+
+  NEWSLETTER: [
+    "120363401981326696@newsletter",
+    "120363419984097704@newsletter"
+  ],
+
   SITES_REF: `
 🌐 Nos sites :
 - https://blackhatvps.store
 - https://panel.blackhatvps.store
 - https://site3.com
-`,
-  MENU_IMG: "https://files.catbox.moe/x7fi39.jpg",
-  CONTACT_ADMIN: {
-    telegram: "https://t.me/LORD_OBITO_DEV",
-    whatsapp: "https://wa.me/2250712668494",
-    phone: "+2250712668494"
-  },
-  MONGO_URI: "mongodb+srv://LORD_OBITO_BOT:QWERTY304@cluster0.5dev5wf.mongodb.net/?retryWrites=true&w=majority"
+`
 };
