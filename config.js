651ff14a76a4a7abb3cd7c535d114be72fc23408
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-09-28 13:37:28 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+export default {
+  BOT_TOKEN: "8133998553:AAGJGie0NxO5Aie_R7cs_cz4UHSFgtkN_Pw",
+  ADMIN_ID: 6587556123,
+  
+  PTERO_URL: "https://panel.tondomaine.com",
+  PTERO_API_KEY: "ptla_P5ajM5606J9cvfqbueud8582blHWqkDevte2NwRNLOq",
+  NEST_ID: 1,
+  EGG_ID: 5,
+  PANEL_DEFAULT_RAM: 0,
+  PANEL_DEFAULT_CPU: 25,
+  PANEL_DEFAULT_DISK: 2048,
+
+  ADSTERA_LINK: "https://tonlien.adstera.com",
+  SITES_REF: `
+🌐 Nos sites :
+- https://site1.com
+- https://site2.com
+- https://site3.com
+`,
+  MENU_IMG: "https://files.catbox.moe/x7fi39.jpg",
+  CONTACT_ADMIN: {
+    telegram: "https://t.me/ObitoDev",
+    whatsapp: "https://wa.me/2256587556123",
+    phone: "+2256587556123"
+  },
+  MONGO_URI: "mongodb+srv://LORD_OBITO_BOT:QWERTY304@cluster0.5dev5wf.mongodb.net/?retryWrites=true&w=majority"
+};
